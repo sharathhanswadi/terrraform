716a6699c22d580054f63aa00b93affa7402cf5c
Your Name <shibir@triconinfotech.com>
2019-10-20 16:53:05 +0530

    local changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,35 +1,35 @@
 
 provider "google" {
- credentials = "${file("circle-ci-demo-256213-18c8f99a4eb2.json")}"
- project     = "flask-app-211918"
- region      = "us-west1"
+  credentials = "${file("circle-ci-demo-256213-18c8f99a4eb2.json")}"
+  project     = "flask-app-211918"
+  region      = "us-west1"
 }
 
 
 resource "random_id" "instance_id" {
- byte_length = 8
+  byte_length = 8
 }
 
 // A single Google Cloud Engine instance
 resource "google_compute_instance" "default" {
- name         = "flask-vm-${random_id.instance_id.hex}"
- machine_type = "f1-micro"
- zone         = "us-west1-a"
+  name         = "flask-vm-${random_id.instance_id.hex}"
+  machine_type = "f1-micro"
+  zone         = "us-west1-a"
 
- boot_disk {
-   initialize_params {
-     image = "debian-cloud/debian-9"
-   }
- }
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
 
-// Make sure flask is installed on all new instances for later steps
- metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python-pip rsync; pip install flask"
+  // Make sure flask is installed on all new instances for later steps
+  metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python-pip rsync; pip install flask"
 
- network_interface {
-   network = "default"
+  network_interface {
+    network = "default"
 
-   access_config {
-     // Include this section to give the VM an external ip address
-   }
- }
+    access_config {
+      // Include this section to give the VM an external ip address
+    }
+  }
 }
