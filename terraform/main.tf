57cd8583e7e88aee0def76a5a578db99d143f1e2
Your Name <shibir@triconinfotech.com>
2019-10-21 18:54:07 +0530

    local changes

---
+++ terraform/main.tf
@@ -0,0 +1,42 @@
+
+provider "google" {
+  credentials = "${file("flask-app-211918-256610-61d6c8f0e5b1.json")}"
+  project = "flask-app-211918-256610"
+  region      = "us-west1"
+}
+
+
+resource "google_project_service" "project" {
+  project = "flask-app-211918-256610"
+  service = "iam.googleapis.com"
+
+  disable_dependent_services = true
+}
+
+resource "random_id" "instance_id" {
+  byte_length = 8
+}
+
+// A single Google Cloud Engine instance
+resource "google_compute_instance" "default" {
+  name         = "flask-vm-${random_id.instance_id.hex}"
+  machine_type = "f1-micro"
+  zone         = "us-west1-b"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1604-xenial-v20170328"
+    }
+  }
+
+  // Make sure flask is installed on all new instances for later steps
+  metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python-pip rsync; pip install flask"
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Include this section to give the VM an external ip address
+    }
+  }
+}
