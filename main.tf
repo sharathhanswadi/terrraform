c643c3fc06493ceffc12a8cf50b9added6c57b81
Your Name <shibir@triconinfotech.com>
2019-10-22 17:42:50 +0530

    local changes

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "random_id" "instance_id" {
 // A single Google Cloud Engine instance
 resource "google_compute_instance" "default" {
   name         = "flask-vm-${random_id.instance_id.hex}"
-  machine_type = "g1-small"
+  machine_type = "f1-micro"
   zone         = "us-west1-b"
 
   boot_disk {
